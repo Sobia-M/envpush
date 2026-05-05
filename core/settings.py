280091e5bdafd897755a80d9c6d7b278af3f0a5c
Sobia-M <sobia5122k@gmail.com>
2026-05-05 22:14:48 +0530

    fix settings for render

--- core/settings.py
+++ core/settings.py
@@ -77,9 +77,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # DATABASES = {
